a716e7127f28a51ef742a02e72e921485395fd14
Kristen Peck <kpeck71@gmail.com>
2017-10-16 19:03:34 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,11 +3,11 @@ class Patient
 
   def initialize(name)
     @name = name
-    Doctor.add_appointment(self)
+    @appointments = []
   end
 
   def add_appointment(appointment)
-    @appointments = []
+
     @appointments << appointment
     appointment.patient = self
   end
