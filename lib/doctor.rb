57daca8931e09b80966a6cf7a5ad21ab6b99199e
Kristen Peck <kpeck71@gmail.com>
2017-10-16 19:02:21 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,7 +6,7 @@ class Doctor
     @appointments = []
   end
 
-  def add_appointment(appointment)
+  def add_appointment
     @appointments << Appointment.new(date)
     appointment.doctor = self
   end
