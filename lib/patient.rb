b0951ca77268d6aac2c75de72a6783a8148b93e7
Kristen Peck <kpeck71@gmail.com>
2017-10-16 18:51:32 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,7 +1,7 @@
 class Patient
   attr_accessor :name, :doctor, :appointment
 
-  def initialize(name,appointment)
+  def initialize(name)
     @name = name
     @appointment = appointment
     appointment.add_patient(self)
