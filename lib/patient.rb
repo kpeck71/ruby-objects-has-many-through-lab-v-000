795078aa09150b305385069e3baf9dfb26b6833e
Kristen Peck <kpeck71@gmail.com>
2017-10-16 19:52:31 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,10 +6,10 @@ class Patient
     @appointments = []
   end
 
-  # def add_appointment(appointment)
-  #   @appointments << appointment
-  #   appointment.patient = self
-  # end
+  def add_appointment(appointment)
+    @appointments << appointment
+    #appointment.patient = self
+  end
 
   def doctors
     @appointments.collect {|app| app.doctor}
